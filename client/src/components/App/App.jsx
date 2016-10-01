2d10b91e55149d406f1f22334d88a80a5eb6d4dd
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-10-01 18:43:40 +0900

    sign in

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
 import YouTube from 'react-youtube';
-import request from 'es6-request';
+import axios from 'axios';
+
+const apiBaseUrl = 'http://localhost:8080';
+const usersUrl = `/users`;
+const signInUrl = `/auth/sign_in`;
+const request = axios.create({ baseURL: apiBaseUrl });
 
 export default class App extends Component {
-  /*
-   * Component render()
-   * see: https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-   */
-  componentDidMount() {
-    this.goToNextVideo();
-  }
+    componentDidMount() {
+        this.setState({
+            user: null,
+            identity: {
+                email: "",
+                password: "",
+            },
+        });
+    }
 
-  goToNextVideo() {
-    request.get("http://localhost:4567/video/next")
-           .perform().then();
-  }
+    handleSignIn() {
+        if (this.state.user == null) {
+            request.post(signInUrl, this.state.identity)
+                   .then((response) => {
+                        ['access-token', 'token-type', 'client', 'expiry', 'uid'].forEach((headerName)=>{
+                            request.defaults.headers.common[headerName] = response.headers[headerName];
+                        });
+                        this.setState({
+                            user: response.data,
+                        });
+                    }).catch((error) => {
+                        console.log(error);
+                    })
+        }
+    }
 
-  render() {
-    return (
-      <YouTube
-        videoId={this.state && this.state.videoId}
-        onEnd={()=>{ this.goToNextVideo(); }}
-        opts={{ playerVars: { autoplay: 1 } }}
-      />
-    );
-  }
+    render() {
+        console.log(this.state);
+        return (
+            <MuiThemeProvider>
+                { this.state !== null && this.state.user === null &&
+                    <Paper style={{ display: 'inline-block' }}>
+                        <TextField
+                            floatingLabelText='email'
+                            onChange={(evt, value) => { this.setState({ identity: { email: value, password: this.state.identity.password } }) }}
+                        /><br />
+                        <TextField
+                            floatingLabelText='password'
+                            type='password'
+                            onChange={(evt, value) => { this.setState({ identity: { email: this.state.identity.email, password: value } }) }}
+                        />
+                        <RaisedButton label='Sign In' onClick={() => { this.handleSignIn(); }} />
+                    </Paper>
+                }
+            </MuiThemeProvider>
+        );
+    }
 }
