19d5c7f8418f3490dbc661f25ddcc7e9f3d37f37
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-10-01 13:06:04 +0900

    railsに乗り換え

--- server/createdb.rb
+++
@@ -1,9 +0,0 @@
-require 'sequel'
-DB = Sequel.sqlite('development.sqlite3')
-
-DB.create_table :videos do
-  primary_key :id
-  String :video_id, null: false
-  DateTime :created_at, null: false
-  DateTime :played_at
-end
--- server/seeds.rb
+++
@@ -1,6 +0,0 @@
-require 'sequel'
-DB = Sequel.sqlite('development.sqlite3')
-
-['3y8UZylK8L0', 'NEb9MTNk3y4', 'tr-ALJS48WY', 'KYOvPZH8tpo'].each do |video_id|
-  DB[:videos].insert(video_id: video_id, created_at: Time.now)
-end
