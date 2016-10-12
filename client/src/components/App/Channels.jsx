a0068dce611dcc8e08e1124b94f20d0d40abeeb8
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-10-12 13:54:24 +0900

    creation of new channel

--- client/src/components/App/Channels.jsx
+++ client/src/components/App/Channels.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
 import Player from './Player'
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const channelsUrl = '/channels'
 
@@ -9,6 +11,7 @@ export default class Queue extends Component {
     componentDidMount () {
         this.setState({
             target_channel: null,
+            new_channel_name: '',
             channels: [],
         });
         this.handleFetchChannels();
@@ -21,6 +24,14 @@ export default class Queue extends Component {
                   })
     }
 
+    handleCreateChannel(name) {
+        this.props.request.post(channelsUrl, { channel: { name } })
+                  .then((response)=> {
+                     this.setState({ new_channel_name: '' });
+                     this.handleFetchChannels();
+                  });
+    }
+
     render() {
         let content = null;
         if (this.state !== null) {
@@ -28,6 +39,13 @@ export default class Queue extends Component {
                 content = (
                     <Paper>
                         <List>
+                            <ListItem disabled={true}>
+                                <TextField
+                                    floatingLabelText='channel name'
+                                    onChange={(evt, value) => { this.setState({ new_channel_name: value }); }}
+                                />
+                                <RaisedButton label='Create' onClick={this.handleCreateChannel.bind(this, this.state.new_channel_name)} />
+                            </ListItem>
                             {
                                 this.state.channels.map((channel) => {
                                     return (
